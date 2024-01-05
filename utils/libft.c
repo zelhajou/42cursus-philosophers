86abbb64fa55066f2316b4116dc937acb79a3c6c
Zakaria Elhajoui <zakaria.elhajoui1@gmail.com>
2024-01-05 14:34:19 +0100

    Create libft.c

---
+++ utils/libft.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zelhajou <zelhajou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 20:49:19 by zelhajou          #+#    #+#             */
+/*   Updated: 2024/01/05 14:01:09 by zelhajou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	while (*s != '\0')
+	{
+		write(fd, s, 1);
+		s++;
+	}
+}
+
+int	skip_space(int *i, const char *str)
+{
+	int	sign;
+
+	sign = 1;
+	while ((str[*i] >= 9 && str[*i] <= 13) || str[*i] == 32)
+		(*i)++;
+	if (str[*i] == '-' || str[*i] == '+')
+	{
+		if (str[*i] == '-')
+			sign *= -1;
+		(*i)++;
+	}
+	return (sign);
+}
+
+int	ft_atoi(const char *str)
+{
+	int			sign;
+	int			i;
+	long long	res;
+	long long	prev;
+	long long	tmp;
+
+	i = 0;
+	res = 0;
+	sign = skip_space(&i, str);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		prev = res;
+		res = res * 10 + str[i] - '0';
+		tmp = res / 10;
+		if (tmp != prev)
+		{
+			if (sign == 1)
+				return (-1);
+			else
+				return (0);
+		}
+		i++;
+	}
+	return (sign * res);
+}
+
+long	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+}
